eb7bba8e9fd28eec94a65fb2136be8c4e6e06a73
Amogh <141360272+AmoghRau@users.noreply.github.com>
2026-09-10 02:12:14 +0530

    Add files via upload

--- scripts/asset_validator.py
+++ scripts/asset_validator.py
@@ -1,15 +1,38 @@
-import bpy 
+#Asset Validator
 
-poly_limit = 1000
+import bpy
+
+#validation settings
+POLYGON_LIMIT = 1000
+
+print("\n ==== AMOGH ASSET VALIDATOR ====")
 
 for obj in bpy.context.selected_objects:
-    if obj.type == 'MESH' :
+    #only validate mesh objects
+    if obj.type != 'MESH':
+        continue
+    issues = []
+    
+    #poly count checker
+    polygon_count = len(obj.data.polygons)
+    
+    if polygon_count > POLYGON_LIMIT:
+        issues.append(f"Too many polygons ({polygon_count})")
         
-        #counting the polygons
-        poly_count = len(obj.data.polygons)
+    #material checker
+    mat_count = len(obj.data.materials)
+    if mat_count == 0:
+        issues.append("No material")
+    
+    #printing result
+    if len(issues) == 0:
+        print(f" ✓ {obj.name}: READY ")
+    else:
+        print(f" ⚠ {obj.name} : NEEDS ATTENTION ")
+        for issues in issues:
+            print(f"    - {issues}")
+
+print("==== VALIDATION COMPLETE ====")
+       
         
-        #deciding whether the asset is okay or not
-        if poly_count <= poly_limit :
-            print(f" ✓ {obj.name}: {poly_count} polygons - GOOD")
-        else:
-            print(f"⚠ {obj.name} : {poly_count} polygons - TOO HIGH")
+    
